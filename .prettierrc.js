ad0a619b5ade4845916df6d17659c91183f0b24b
Mario Muniz <mario@mariomuniz.com>
2022-08-11 22:57:57 -0700

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  importOrder: [
+    '^react$|^react-native/?(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
